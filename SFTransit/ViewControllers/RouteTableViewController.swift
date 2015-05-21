dddec3d4030d9b963cc9e147c1f74fb1a399a73c
Amie Kweon <amie.kweon@rd.io>
2015-05-21 01:18:43 -0700

    Deselect rows upon selection in routes table

--- SFTransit/ViewControllers/RouteTableViewController.swift
+++ SFTransit/ViewControllers/RouteTableViewController.swift
@@ -64,5 +64,7 @@ extension RouteTableViewController: UITableViewDataSource {
 }
 
 extension RouteTableViewController: UITableViewDelegate {
-    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
